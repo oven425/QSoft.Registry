fe702f6fd593a459bdf7cc706558b8ec4ca0e307
Ben_Hsu <ben_hsu@ubiqconn.com>
2021-01-07 16:52:20 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using QSoft.Reg.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ll = new List<int>();
+            //ll.Where
+            RegistryKey reg = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey uninstall = reg.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+            //foreach (var oo in uninstall.GetSubKeyNames())
+            //{
+            //    RegistryKey subkey = uninstall.OpenSubKey(oo);
+            //    string displayname = subkey.GetValue("DisplayName") as string;
+            //    System.Diagnostics.Trace.WriteLine(displayname);
+            //    //subkey.Dispose();
+            //}
+            //var vv = uninstall.Where(x => x.GetValue<string>("DisplayName")== "Intel(R) Processor Graphics");
+            //foreach(var oo in vv)
+            //{
+
+            //}
+
+            var vv = from regt in uninstall where reg.GetValue<string>("DisplayName") == "Intel(R) Processor Graphics" select regt;
+            foreach (var oo in vv)
+            {
+
+            }
+        }
+    }
+}
---
+++ QSoft.Reg/RegistryKeyEx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace QSoft.Reg.Linq
+{
+    public static class RegistryKeyEx
+    {
+        public static IEnumerable<RegistryKey> Where(this RegistryKey src, Func<RegistryKey, bool> func)
+        {
+            List<RegistryKey> ll = new List<RegistryKey>();
+            string[] subkeynames = src.GetSubKeyNames();
+            foreach(var subkeyname in subkeynames)
+            {
+                RegistryKey reg = src.OpenSubKey(subkeyname);
+                if(func.Invoke(reg) == true)
+                {
+                    ll.Add(reg);
+                }
+                else
+                {
+                    reg.Dispose();
+                    reg = null;
+                }
+            }
+            return ll;
+        }
+
+        public static int Count(this RegistryKey src, Func<RegistryKey, bool> func)
+        {
+            int count = 0;
+            string[] subkeynames = src.GetSubKeyNames();
+            foreach (var subkeyname in subkeynames)
+            {
+                RegistryKey reg = src.OpenSubKey(subkeyname);
+                if (func.Invoke(reg) == true)
+                {
+                    count++;
+                }
+                else
+                {
+                    reg.Dispose();
+                    reg = null;
+                }
+            }
+            return count;
+        }
+
+        public static T GetValue<T>(this RegistryKey src, string name)
+        {
+            T t =  default(T);
+            if(src.GetValueNames().Any(x=>x==name) == true)
+            {
+                object obj = src.GetValue(name);
+                t = (T)obj;
+            }
+            else
+            {
+                if(Type.GetTypeCode(typeof(T)) == TypeCode.String)
+                {
+                    object obj = string.Empty;
+                    t = (T)obj;
+                }
+            }
+            return t;
+        }
+    }
+}

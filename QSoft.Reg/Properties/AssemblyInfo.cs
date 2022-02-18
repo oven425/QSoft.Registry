d527f7a5f54fe8ef833aa69189f0a21e6898329a
oven425 <oven425@yahoo.com.tw>
2022-02-18 17:50:34 +0800

    update

--- QSoft.Reg/Properties/AssemblyInfo.cs
+++ QSoft.Reg/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("QSoft.Registry")]
+[assembly: AssemblyTitle("QSoft.RegistryKey")]
 [assembly: AssemblyDescription("Linq to RegistryKey")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QSoft.Registry")]
+[assembly: AssemblyProduct("QSoft.RegistryKey")]
 [assembly: AssemblyCopyright("Copyright © Ben Hsu 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，或將組建編號或修訂編號設為預設值
 //方法是使用 '*'，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]

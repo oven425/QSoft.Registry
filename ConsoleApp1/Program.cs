5b5834bc8eb30c72f252362ee35f54cdd5abb3e2
oven425 <oven425@yahoo.com.tw>
2022-03-01 18:19:30 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Security;
+using System.Security.AccessControl;
 using System.Threading;
 using Microsoft.Win32;
 using QSoft.Registry;
@@ -158,6 +160,96 @@ class Program
             //apps.Add(new App() { Name = "DD", Offical = false });
 
 #if Queryable
+            string user = Environment.UserDomainName + "\\" + Environment.UserName;
+            RegistrySecurity rs = new RegistrySecurity();
+
+            // Allow the current user to read and delete the key.
+            //
+            rs.AddAccessRule(new RegistryAccessRule(user,
+                RegistryRights.ReadKey | RegistryRights.Delete,
+                InheritanceFlags.None,
+                PropagationFlags.None,
+                AccessControlType.Allow));
+
+            rs.AddAccessRule(new RegistryAccessRule(user,
+            RegistryRights.WriteKey | RegistryRights.ReadKey | RegistryRights.Delete | RegistryRights.ChangePermissions,
+            InheritanceFlags.None,
+            PropagationFlags.None,
+            AccessControlType.Deny));
+
+            // Create the example key with registry security.
+            RegistryKey rk = null;
+            try
+            {
+                rk = Registry.CurrentUser.CreateSubKey("RegistryRightsExample",
+                    RegistryKeyPermissionCheck.Default, rs);
+                Console.WriteLine("\r\nExample key created.");
+                rk.SetValue("ValueName", "StringValue");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\r\nUnable to create the example key: {0}", ex);
+            }
+            if (rk != null) rk.Close();
+
+
+            rk = Registry.CurrentUser;
+
+            RegistryKey rk2;
+
+            // Open the key with read access.
+            rk2 = rk.OpenSubKey("RegistryRightsExample", false);
+            Console.WriteLine("\r\nRetrieved value: {0}", rk2.GetValue("ValueName"));
+            rk2.Close();
+
+            // Attempt to open the key with write access.
+            try
+            {
+                rk2 = rk.OpenSubKey("RegistryRightsExample", true);
+            }
+            catch (SecurityException ex)
+            {
+                Console.WriteLine("\nUnable to write to the example key." +
+                    " Caught SecurityException: {0}", ex.Message);
+            }
+            if (rk2 != null) rk2.Close();
+
+            // Attempt to change permissions for the key.
+            try
+            {
+                rs = new RegistrySecurity();
+                rs.AddAccessRule(new RegistryAccessRule(user,
+                    RegistryRights.WriteKey,
+                    InheritanceFlags.None,
+                    PropagationFlags.None,
+                    AccessControlType.Allow));
+                rk2 = rk.OpenSubKey("RegistryRightsExample", false);
+                rk2.SetAccessControl(rs);
+                Console.WriteLine("\r\nExample key permissions were changed.");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("\nUnable to change permissions for the example key." +
+                    " Caught UnauthorizedAccessException: {0}", ex.Message);
+            }
+            if (rk2 != null) rk2.Close();
+
+            Console.WriteLine("\r\nPress Enter to delete the example key.");
+            Console.ReadLine();
+
+            try
+            {
+                rk.DeleteSubKey("RegistryRightsExample");
+                Console.WriteLine("Example key was deleted.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to delete the example key: {0}", ex);
+            }
+
+            rk.Close();
+
+
 
             InstalledApp installedapp = new InstalledApp();
             var pps = typeof(InstalledApp).GetProperties().Where(x=>x.CanRead==true&&x.CanWrite == true);

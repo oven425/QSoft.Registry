623062428d344c428a68ab9be07f6acc8c7ea5ad
oven425 <oven425@yahoo.com.tw>
2022-03-08 17:48:12 +0800

    update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,37 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
+    //study http://csharpexamples.com/c-binary-search-tree-implementation/
     class Program
     {
         static void Main(string[] args)
         {
-            A a = new A();
-            a.AA(10);
-            B<string> b = new B<string>();
-            b.AA("10");
-        }
-    }
+            Node node = new Node { Value = 1 };
 
-    public class A
-    {
-        public void AA(int data)
+        }
+
+        static void Insert()
         {
-
+            
         }
     }
 
-    public class B<T>:A
+    public class BinaryTree
     {
-        public void AA(T data)
+        public Node Root { set; get; }
+        public void Insert(int data)
         {
+            if(this.Root == null)
+            {
+                this.Root = new Node() { Value = data };
+                return;
+            }
+            while(true)
+            {
+                if(this.Root.Value >data)
+                {
 
+                }
+                else
+                {
+
+                }
+            }
         }
     }
+    
+
+    public class Node
+    {
+        public Node Left { set; get; }
+        public Node Right { set; get; }
+        public int Value { set; get; }
+    }
+
+
 
 }
